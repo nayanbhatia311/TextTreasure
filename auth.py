0523d4455cc023551906c6a8237d8a93ecee23d2
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-22 04:07:14 -0700

    create register user api

--- auth.py
+++ auth.py
@@ -1,8 +1,18 @@
-from flask import Blueprint,jsonify
-
+from flask import Blueprint,jsonify,request
+from models import User
 auth_bp=Blueprint('auth',__name__)
 
 @auth_bp.post('/register')
 def register_user():
+	data=request.get_json()
+	
+	user = User.get_user_by_username(username=data.get("username"))
+
+	if user:
+		return jsonify({"error":"User already exist"})
+	
+	new_user= User(username=data.get("username"),email=data.get("email"))
+	new_user.set_password(password=data.get('password'))
+	new_user.save()
 	return jsonify({"message":"User created"})
 
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from extensions import db
 from uuid import uuid4
+from werkzeug.security import generate_password_hash,check_password_hash
+
 class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.String(),primary_key=True, default=str(uuid4))
@@ -10,4 +12,22 @@ class User(db.Model):
 	def __repr__(self):
 		return f"User: {self.username}, Email: {self.email}"
 	
+	def set_password(self,password):
+		self.password=generate_password_hash(password)
+	
+	def check_password(self,password):
+		return check_password_hash(self.password,password)
+	
+
+	@classmethod
+	def get_user_by_username(cls,username):
+		return cls.query.filter_by(username=username).first()
+
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
+	
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()	
 	
