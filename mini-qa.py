491f8ad7c54a56aec11db6ac3e783f8247612008
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-23 16:43:33 -0700

    add astra db support

---
+++ mini-qa.py
@@ -0,0 +1,27 @@
+ASTRA_DB_SECURE_BUNDLE_PATH=""
+ASTRA_DB_APPLICATION_TOKEN=""
+ASTRA_DB_CLIENT_ID=""
+ASTRA_DB_SECRET=""
+ASTRA_DB_KEYSPACE=""
+OPENAI_API_KEY=""
+
+from langchain.vectorstores.cassandra import Cassandra
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+from datasets import load_datasets
+
+#connect to astra db
+
+cloud_config = {
+'secure_connect_bundle': ASTRA_DB_SECURE_BUNDLE_PATH
+}
+
+auth_provider = PlainTextAuthProvider(ASTRA_DB_CLIENT_ID,ASTRA_DB_CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+astraSession= cluster.connect()
+
