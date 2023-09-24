5b849523bebd8ff433a1606d3068eec190ae9f5f
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-23 22:51:34 -0700

    fix the env variable bug

--- mini-qa.py
+++ mini-qa.py
@@ -1,10 +1,6 @@
-ASTRA_DB_SECURE_BUNDLE_PATH=""
-ASTRA_DB_APPLICATION_TOKEN=""
-ASTRA_DB_CLIENT_ID=""
-ASTRA_DB_SECRET=""
-ASTRA_DB_KEYSPACE=""
-OPENAI_API_KEY=""
 
+from dotenv import load_dotenv
+import os
 from langchain.vectorstores.cassandra import Cassandra
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
@@ -13,19 +9,26 @@ from langchain.embeddings import OpenAIEmbeddings
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
-from datasets import load_datasets
-
+from datasets import load_dataset
+load_dotenv()
 #connect to astra db
+
+ASTRA_DB_SECURE_BUNDLE_PATH = os.getenv("ASTRA_DB_SECURE_BUNDLE_PATH")
+ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_CLIENT_ID = os.getenv("ASTRA_DB_CLIENT_ID")
+ASTRA_DB_SECRET = os.getenv("ASTRA_DB_SECRET")
+ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 cloud_config = {
 'secure_connect_bundle': ASTRA_DB_SECURE_BUNDLE_PATH
 }
 
-auth_provider = PlainTextAuthProvider(ASTRA_DB_CLIENT_ID,ASTRA_DB_CLIENT_SECRET)
+auth_provider = PlainTextAuthProvider(ASTRA_DB_CLIENT_ID,ASTRA_DB_SECRET)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
 astraSession= cluster.connect()
 
-llm = OPENAI(openai_api_key=OPENAI_API_KEY)
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 myEmbedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 myCassandraVStore = Cassandra(
@@ -38,25 +41,25 @@ myCassandraVStore = Cassandra(
 
 print("load data from huggingface")
 myDataset=load_dataset("Biddls/Onion_News", split="train")
-headlines = myDatasets["text"][:50]
+headlines = myDataset["text"][:50]
 
 print("Generate embedding and store in AstraDb")
 myCassandraVStore.add_texts(headlines)
 
-print(f"Inserted {len(headlines} headlines")
+print(f"Inserted {len(headlines)} headlines")
 vectorIndex= VectorStoreIndexWrapper(vectorstore=myCassandraVStore)
 
 
 while True:
-	query_text=input("Enter question (type 'quit' to exit")
+	query_text=input("\nEnter question (type 'quit' to exit)\n")
 	
 	if query_text.lower()=="quit":
 		break
 
 	print("Question: ",query_text)
-	answer=vectorIndex.query(quert_text,llm=llm).strip()
+	answer=vectorIndex.query(query_text,llm=llm).strip()
 	print(f"Answer:{answer}")
 
 	print("Document by relevance:")
-	for doc,store in myCassandraVStore.similarity_search_with_score(query_text,k=4):
+	for doc,score in myCassandraVStore.similarity_search_with_score(query_text,k=2):
 		print(score,doc.page_content)
