7652589815e7cf678c6e2f1f3f87c4d120812413
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-09-25 19:21:53 -0700

    add jwt and refresh token

--- auth.py
+++ auth.py
@@ -1,18 +1,43 @@
-from flask import Blueprint,jsonify,request
+from flask import Blueprint, jsonify, request
 from models import User
-auth_bp=Blueprint('auth',__name__)
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+auth_bp = Blueprint('auth', __name__)
+
 
 @auth_bp.post('/register')
 def register_user():
-	data=request.get_json()
-	
-	user = User.get_user_by_username(username=data.get("username"))
+    data = request.get_json()
 
-	if user:
-		return jsonify({"error":"User already exist"}), 403
-	
-	new_user= User(username=data.get("username"),email=data.get("email"))
-	new_user.set_password(password=data.get('password'))
-	new_user.save()
-	return jsonify({"message":"User created"}), 201
+    user = User.get_user_by_username(username=data.get("username"))
 
+    if user:
+        return jsonify({"error": "User already exist"}), 403
+
+    new_user = User(username=data.get("username"), email=data.get("email"))
+    new_user.set_password(password=data.get('password'))
+    new_user.save()
+    return jsonify({"message": "User created"}), 201
+
+
+@auth_bp.post('/login')
+def login_user():
+    data = request.get_json()
+    user = User.get_user_by_username(username=data.get('username'))
+
+    if user and (user.check_password(password=data.get('password'))):
+        access_token = create_access_token(identity=user.username)
+        refresh_token = create_refresh_token(identity=user.username)
+        return jsonify(
+            {
+                "message": "Logged in",
+                "tokens": {
+                    "access": access_token,
+                    "refresh": refresh_token
+                }
+            }
+        ), 200
+    return jsonify({
+        "error": "Invalid password"
+    }
+    ), 400
